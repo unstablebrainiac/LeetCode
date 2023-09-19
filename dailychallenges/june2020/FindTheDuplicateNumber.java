b5cd159c3bb1935b6d2713a698970ccf20cb7e78
unstablebrainiac <sajalnarang@gmail.com>
2023-09-18 20:29:30 -0400

    Daily challenge 20230919

--- dailychallenges/june2020/FindTheDuplicateNumber.java
+++ dailychallenges/june2020/FindTheDuplicateNumber.java
@@ -1,18 +1,18 @@
 package dailychallenges.june2020;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /*
  * https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/542/week-4-june-22nd-june-28th/3371/
  */
 public class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return nums[i];
-                }
-            }
-        }
-        throw new IllegalStateException();
+        HashSet<Object> numSet = new HashSet<>();
+        return Arrays.stream(nums)
+                .filter(num -> !numSet.add(num))
+                .findFirst()
+                .orElseThrow();
     }
 
     public static void main(String[] args) {
