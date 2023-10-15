09ea5b79bf3b0e2c270322bd948327085c34fca9
unstablebrainiac <sajalnarang@gmail.com>
2023-10-15 01:20:09 -0400

    Weekly contest 367

---
+++ weeklycontest367/ConstructProductMatrix.java
@@ -0,0 +1,36 @@
+package weeklycontest367;
+
+import java.util.Arrays;
+
+/*
+ * https://leetcode.com/problems/construct-product-matrix/
+ */
+public class ConstructProductMatrix {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int[][] ans = new int[grid.length][grid[0].length];
+        long[][] postProducts = new long[grid.length][grid[0].length];
+        long pre = 1, post = 1;
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[i].length - 1; j >= 0; j--) {
+                postProducts[i][j] = post;
+                post = (post * grid[i][j]) % 12345;
+            }
+        }
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                ans[i][j] = (int) ((pre * postProducts[i][j]) % 12345);
+                pre = (pre * grid[i][j]) % 12345;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(new ConstructProductMatrix().constructProductMatrix(
+                new int[][]{
+                        {1, 2},
+                        {3, 4},
+                }
+        ))); // [[24, 12], [8, 6]]
+    }
+}
---
+++ weeklycontest367/FindIndicesWithIndexAndValueDifference1.java
@@ -0,0 +1,30 @@
+package weeklycontest367;
+
+import java.util.Arrays;
+
+/*
+ * https://leetcode.com/problems/find-indices-with-index-and-value-difference-i/
+ */
+public class FindIndicesWithIndexAndValueDifference1 {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        if (indexDifference == 0 && valueDifference == 0) {
+            return new int[]{0, 0};
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + indexDifference; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) >= valueDifference) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new FindIndicesWithIndexAndValueDifference1().findIndices(
+                new int[]{5, 1, 4, 1},
+                2,
+                4
+        ))); // [0,3]
+    }
+}
