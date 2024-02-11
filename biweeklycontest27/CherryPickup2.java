c2e27c6032fe51f654168808b639cfaf35f2c910
unstablebrainiac <sajalnarang@gmail.com>
2024-02-11 00:14:58 -0500

    Daily challenge 20240211

--- biweeklycontest27/CherryPickup2.java
+++ biweeklycontest27/CherryPickup2.java
@@ -7,97 +7,104 @@ import java.util.Arrays;
  */
 public class CherryPickup2 {
     public int cherryPickup(int[][] grid) {
-        return cherryPickup(grid, 0, 0, grid[0].length - 1);
+        return cherryPickup(grid, 0, 0, grid[0].length - 1, new Integer[grid.length][grid[0].length][grid[0].length]);
     }
 
-    private int cherryPickup(int[][] grid, int currentRow, int index1, int index2) {
+    private int cherryPickup(int[][] grid, int currentRow, int index1, int index2, Integer[][][] dp) {
         if (index1 > index2) {
-            return cherryPickup(grid, currentRow, index2, index1);
+            return cherryPickup(grid, currentRow, index2, index1, dp);
+        }
+        if (dp[currentRow][index1][index2] != null) {
+            return dp[currentRow][index1][index2];
         }
         int[] curRow = grid[currentRow];
         if (currentRow == grid.length - 1) {
             if (index1 == index2) {
                 return curRow[index1];
             }
-            return curRow[index1] + curRow[index2];
+
+            int ans = curRow[index1] + curRow[index2];
+            dp[currentRow][index1][index2] = ans;
+            return ans;
         }
-        int maximum = Integer.MIN_VALUE;
+        int maximum;
         if (index1 == index2) {
             if (index1 == 0) {
                 maximum = maximum(
-                        cherryPickup(grid, currentRow + 1, 0, 0),
-                        cherryPickup(grid, currentRow + 1, 0, 1),
-                        cherryPickup(grid, currentRow + 1, 1, 1)
+                        cherryPickup(grid, currentRow + 1, 0, 0, dp),
+                        cherryPickup(grid, currentRow + 1, 0, 1, dp),
+                        cherryPickup(grid, currentRow + 1, 1, 1, dp)
                 );
             } else if (index1 == curRow.length - 1) {
                 maximum = maximum(
-                        cherryPickup(grid, currentRow + 1, index1, index1),
-                        cherryPickup(grid, currentRow + 1, index1 - 1, index1),
-                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 - 1)
+                        cherryPickup(grid, currentRow + 1, index1, index1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 - 1, index1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 - 1, dp)
                 );
             } else {
                 maximum = maximum(
-                        cherryPickup(grid, currentRow + 1, index1, index1),
-                        cherryPickup(grid, currentRow + 1, index1, index1 + 1),
-                        cherryPickup(grid, currentRow + 1, index1 - 1, index1),
-                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 + 1),
-                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 - 1),
-                        cherryPickup(grid, currentRow + 1, index1 + 1, index1 + 1)
+                        cherryPickup(grid, currentRow + 1, index1, index1, dp),
+                        cherryPickup(grid, currentRow + 1, index1, index1 + 1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 - 1, index1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 + 1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 - 1, index1 - 1, dp),
+                        cherryPickup(grid, currentRow + 1, index1 + 1, index1 + 1, dp)
                 );
             }
-            return curRow[index1] + maximum;
+            int ans = curRow[index1] + maximum;
+            dp[currentRow][index1][index2] = ans;
+            return ans;
         }
         if (index1 == 0 && index2 == curRow.length - 1) {
             maximum = maximum(
-                    cherryPickup(grid, currentRow + 1, 0, index2),
-                    cherryPickup(grid, currentRow + 1, 0, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, 1, index2),
-                    cherryPickup(grid, currentRow + 1, 1, index2 - 1)
+                    cherryPickup(grid, currentRow + 1, 0, index2, dp),
+                    cherryPickup(grid, currentRow + 1, 0, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, 1, index2 - 1, dp)
             );
         } else if (index1 == 0) {
             maximum = maximum(
-                    cherryPickup(grid, currentRow + 1, 0, index2),
-                    cherryPickup(grid, currentRow + 1, 0, index2 + 1),
-                    cherryPickup(grid, currentRow + 1, 0, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, 1, index2),
-                    cherryPickup(grid, currentRow + 1, 1, index2 + 1),
-                    cherryPickup(grid, currentRow + 1, 1, index2 - 1)
+                    cherryPickup(grid, currentRow + 1, 0, index2, dp),
+                    cherryPickup(grid, currentRow + 1, 0, index2 + 1, dp),
+                    cherryPickup(grid, currentRow + 1, 0, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, 1, index2 + 1, dp),
+                    cherryPickup(grid, currentRow + 1, 1, index2 - 1, dp)
             );
         } else if (index2 == curRow.length - 1) {
             maximum = maximum(
-                    cherryPickup(grid, currentRow + 1, index1, index2),
-                    cherryPickup(grid, currentRow + 1, index1, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, index1 - 1, index2),
-                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, index1 + 1, index2),
-                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 - 1)
+                    cherryPickup(grid, currentRow + 1, index1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 - 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 + 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 - 1, dp)
             );
         } else {
             maximum = maximum(
-                    cherryPickup(grid, currentRow + 1, index1, index2),
-                    cherryPickup(grid, currentRow + 1, index1, index2 + 1),
-                    cherryPickup(grid, currentRow + 1, index1, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, index1 - 1, index2),
-                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 + 1),
-                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 - 1),
-                    cherryPickup(grid, currentRow + 1, index1 + 1, index2),
-                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 + 1),
-                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 - 1)
+                    cherryPickup(grid, currentRow + 1, index1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1, index2 + 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 - 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 + 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 - 1, index2 - 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 + 1, index2, dp),
+                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 + 1, dp),
+                    cherryPickup(grid, currentRow + 1, index1 + 1, index2 - 1, dp)
             );
         }
-        return curRow[index1] + curRow[index2] + maximum;
+        int ans = curRow[index1] + curRow[index2] + maximum;
+        dp[currentRow][index1][index2] = ans;
+        return ans;
     }
 
     private int maximum(int... values) {
-        return Arrays.stream(values).max().getAsInt();
+        return Arrays.stream(values).max().orElseThrow();
     }
 
     public static void main(String[] args) {
-//        System.out.println(new CherryPickup2().cherryPickup(
-//                new int[][]{{1, 0, 0, 3}, {0, 0, 0, 3}, {0, 0, 3, 3}, {9, 0, 3, 3}}
-//        )); // 22
         System.out.println(new CherryPickup2().cherryPickup(
                 new int[][]{{3, 1, 1}, {2, 5, 1}, {1, 5, 5}, {2, 1, 1}}
-        )); // 22
+        )); // 24
     }
 }
